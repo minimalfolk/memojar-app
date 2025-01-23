4e51c31261a92b54db71d85f4822c3da8e35873b
minimalfolk <office.minimalclub@gmail.com>
2025-01-23 06:45:43 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,18 @@
-// Toggle Mobile Menu
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+// Navigation Buttons
+document.getElementById('home-icon').addEventListener('click', () => {
+  document.getElementById('public-memories').classList.remove('hidden');
+  document.getElementById('add-memory').classList.add('hidden');
+  document.getElementById('messages-page').classList.add('hidden');
+});
 
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
+document.getElementById('add-icon').addEventListener('click', () => {
+  document.getElementById('add-memory').classList.remove('hidden');
+  document.getElementById('public-memories').classList.add('hidden');
+  document.getElementById('messages-page').classList.add('hidden');
+});
+
+document.getElementById('message-icon').addEventListener('click', () => {
+  document.getElementById('messages-page').classList.remove('hidden');
+  document.getElementById('public-memories').classList.add('hidden');
+  document.getElementById('add-memory').classList.add('hidden');
 });
